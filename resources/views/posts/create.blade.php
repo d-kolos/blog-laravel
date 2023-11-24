13b847f8b8e4ada176a7e31c882acb2d96c4df6a
Dmitriy Kolos <d.kolos.mail@gmail.com>
2023-11-24 23:02:01 +0200

    d08 - create validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,16 +4,21 @@
         @csrf
         <div class="form-group">
             <label for="title">Title</label>
-            <input id="title" type="text" name="title" class="form-control" >
+            <input id="title" type="text" name="title" class="form-control" value="{{ old('title') }}">
         </div>
+        @error('title')<p class="text-danger"><small>{{ $message }}</small></p>@enderror
         <div class="form-group">
             <label for="description">Description</label>
-            <textarea id="description" type="text" name="description" class="form-control" ></textarea>
+            <textarea id="description" type="text" name="description" class="form-control"
+            >{{ old('description') }}</textarea>
         </div>
+        @error('description')<p class="text-danger"><small>{{ $message }}</small></p>@enderror
         <div class="form-group">
             <label for="content">Content</label>
-            <textarea id="content" type="text" name="content" rows="7" class="form-control" ></textarea>
+            <textarea id="content" type="text" name="content" rows="7" class="form-control"
+            >{{ old('content') }}</textarea>
         </div>
+        @error('content')<p class="text-danger"><small>{{ $message }}</small></p>@enderror
         <button class="btn btn-success my-4">Create</button>
     </form>
 </x-layouts.main>
