8aac23fbb3a453aee0c19a8fde876089422a9e0b
Dmitriy Kolos <d.kolos.mail@gmail.com>
2023-11-28 01:38:01 +0200

    d09 - category - has many;   post - belongs to

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 /**
  * @method static insert(array $array)
@@ -14,4 +15,10 @@ class Post extends Model
 {
     use HasFactory;
     protected $fillable = ['title', 'description', 'content', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
 }
