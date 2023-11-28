1834567abf3e5f53824890f280bbab239c2ec000
Dmitriy Kolos <d.kolos.mail@gmail.com>
2023-11-28 23:00:57 +0200

    d09 - PostFactory defined

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,10 +2,11 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ * @extends Factory<Post>
  */
 class PostFactory extends Factory
 {
@@ -17,7 +18,10 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => rtrim(fake()->sentence(), '.'),
+            'description' => fake()->text(),
+            'content' => fake()->text(800),
+            'category_id' => rand(1, 10),
         ];
     }
 }
